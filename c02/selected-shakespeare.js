3a95663fd3bb0e7f9dc32804fdd9bab201fc236e
Human Inhabitant <human-inhabitant@pm.me>
2020-06-22 19:15:23 -0400

    Completed chapters 1 and 2...

---
+++ c02/selected-shakespeare.js
@@ -0,0 +1,21 @@
+$( document ).ready(() => {
+  $( '#selected-plays > li' ).addClass( 'horizontal' );
+  $( '#selected-plays li:not(.horizontal)' ).addClass( 'sub-level' );
+
+  $( 'a[href^="mailto:"]' ).addClass( 'mailto' );
+  $( 'a[href$=".pdf"]' ).addClass( 'pdflink' );
+  $( 'a[href*="mysite.com"]' ).addClass( 'mysite' );
+
+
+  $( 'th' ).parent().addClass( 'table-heading' );
+  $( 'tr:not([th]):even' ).addClass( 'even' );
+  $( 'tr:not([th]):odd' ).addClass( 'odd' );
+  $( 'td:contains("Henry")' )
+    .parent()
+    .find( 'td:eq(1)' )
+    .addClass( 'highlight' )
+    .end()
+    .find( 'td:eq(2)' )
+    .addClass( 'highlight' )
+  ;
+});
